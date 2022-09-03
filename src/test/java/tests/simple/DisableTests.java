6986903e48e107448383c62fb0f197b4d589e0c2
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-09-03 15:49:29 +0300

    simple tests added

--- src/test/java/tests/simple/DisableTests.java
+++ src/test/java/tests/simple/DisableTests.java
@@ -1,5 +1,6 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class DisableTests {
 
     @Test
+    @Disabled("Testing paused by QA")
     void test1(){
         assertTrue(false);
     }
 
     @Test
+    @Disabled("Testing paused by QA")
     void test2(){
         assertTrue(false);
     }
