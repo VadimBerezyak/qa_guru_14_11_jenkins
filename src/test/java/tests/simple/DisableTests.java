b524230a70f694557d2d1dac73678adc6083a9ef
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-09-03 15:30:05 +0300

    first commit

---
+++ src/test/java/tests/simple/DisableTests.java
@@ -0,0 +1,20 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisableTests {
+
+    @Test
+    void test1(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test2(){
+        assertTrue(false);
+    }
+
+
+}
