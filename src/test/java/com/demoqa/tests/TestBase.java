53c6558488f88c42717473bf3cb86a961d2574d9
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-09-04 20:40:29 +0300

    DemoQA tests: video attachments added

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,15 +1,12 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-
-import static org.openqa.selenium.logging.LogType.BROWSER;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
@@ -18,14 +15,22 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
     }
+
     @AfterEach
-    void addAttachments(){
+    void addAttachments() {
         Attach.screenshotAs("LastScreenshots");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
-
 
 
 }
