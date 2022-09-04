95b629c8d7e5c1f9ebfb59031ad860a38f7d1d3d
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-09-04 20:16:36 +0300

    DemoQA tests: attachments added

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,9 +1,15 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class TestBase {
     @BeforeAll
@@ -13,6 +19,13 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("LastScreenshots");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+    }
+
 
 
 }
